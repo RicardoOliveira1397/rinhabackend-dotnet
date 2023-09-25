2392ba92f1741944cd1d131005e2069f27b47bf4
Ricardo <ricardo@atonsystems.com.br>
2023-09-25 09:22:45 -0300

    clone repository

---
+++ RinhaBackend/IPessoaRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace RinhaBackend;
+
+public interface IPessoaRepository
+{
+    Task Insert(Pessoa pessoa);
+    Task<Pessoa> GetById(Guid id);
+    Task<int> Total();
+    Task<List<Pessoa>> ListByTerm(string s);
+    Task<bool> GetByApelido(string apelido);
+}
---
+++ tests/ddl.sql
@@ -0,0 +1,12 @@
+CREATE UNLOGGED TABLE "Pessoas" (
+    "Id" uuid NOT NULL CONSTRAINT "PK_Pessoas" PRIMARY KEY,
+    "Apelido" character varying(32) NOT NULL,
+    "Nome" character varying(100) NOT NULL,
+    "Nascimento" date NOT NULL,
+    "Stack" character varying(32)[],
+    "Busca" TEXT
+);
+
+CREATE INDEX IF NOT EXISTS IDX_PESSOAS_APELIDO ON "Pessoas" ("Apelido");
+CREATE EXTENSION IF NOT EXISTS PG_TRGM;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS IDX_PESSOAS_BUSCA ON "Pessoas" USING GIST ("Busca" GIST_TRGM_OPS(SIGLEN=64));
